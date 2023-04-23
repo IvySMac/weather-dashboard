abb321e02c6910c036b507a789d68952d722c35e
Creature Feature <ivy.mccurdy@gmail.com>
2023-04-23 16:42:08 -0500

    fetched weather api and console logged the data

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
-console.log("hello world");
+var searchButton = document.getElementById('search-button');
+
+function getApi() {
+
+  var requestUrl = 'http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c2497406e274517dafa1edd628b99c41';
+
+  fetch(requestUrl)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+     console.log(data);
+      }
+    )};
+
+searchButton.addEventListener('click', getApi);
+
+
+
+// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c2497406e274517dafa1edd628b99c41
+// * get search term
+// * send search to API
+// * get results from API
+// * render results to result container
+// *  - each result should include
+// *  - temp
+// *  - weather, 
+//      temp, 
+//      humidity, 
+//      wind speed
+// * - New results should clear old results
+// * - Once results are located, the search term should be emptied
+// * - search revealed below search button
