8a2efacdfbaf2ff2458f202e284e85ae1e1fd069
Creature Feature <ivy.mccurdy@gmail.com>
2023-04-26 18:11:15 -0500

    added the five day forecast with for loop, deleted extra div tags, styled five day weather cards

--- script.js
+++ script.js
@@ -18,9 +18,23 @@ function getFiveDay(id) {
         .then(function (data) {
             console.log(data);
 
-        forecast(data.list[0]);
         for (i = 7; i < 40; i += 8) {
-         forecast(data.list[i]);
+         var forecastData = data.list[i];
+         var forecastDate = forecastData.dt_txt.split(' ')[0];
+         var formattedDate = new Date(forecastDate).toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: 'numeric'});
+         var forecastTemp = forecastData.main.temp.toFixed(0) + '°F';
+         var forecastWind = forecastData.wind.speed + ' mph';
+         var forecastHumidity = forecastData.main.humidity + '%';
+
+         var forecastCard = $('<div>').addClass('card');
+         var forecastCardBody = $('<div>').addClass('card-body');
+         var forecastDateEl = $('<h5>').addClass('card-title').text(formattedDate);
+         var forecastTempEl = $('<p>').addClass('card-text').text('Temperature: ' + forecastTemp);
+         var forecastWindEl = $('<p>').addClass('card-text').text('Wind Speed: ' + forecastWind);
+         var forecastHumidityEl = $('<p>').addClass('card-text').text('Humidity: ' + forecastHumidity);
+         forecastCardBody.append(forecastDateEl, forecastTempEl, forecastWindEl, forecastHumidityEl);
+         forecastCard.append(forecastCardBody);
+         $('#forecast').append(forecastCard);
         }
         
         }
@@ -40,8 +54,8 @@ function getWeatherResults(lat, lon) {
             var cardTitle = $('<h3>').addClass('card-title').text(data.name)
             var cardBody = $('<div>').addClass('card-body')
             var tempEl = $('<p>').addClass('card-text').text('Temp: ' + data.main.temp.toFixed(0) + "°F")
-            var windEl = $('<p>').addClass('card-text').text('Wind Speed: ' + data.wind.speed)
-            var humidityEl = $('<p>').addClass('card-text').text('Humidity: ' + data.main.humidity)
+            var windEl = $('<p>').addClass('card-text').text('Wind Speed: ' + data.wind.speed+ " mph")
+            var humidityEl = $('<p>').addClass('card-text').text('Humidity: ' + data.main.humidity+ "%")
             var iconCode =  $('<img>').addClass('card-img-top').attr('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png').attr('alt', 'Weather icon').attr('style', 'width: 55px', 'height: 55px');
             $('.weather').append(card.append(cardTitle, iconCode, cardBody, tempEl, windEl, humidityEl))
             getFiveDay(data.id);
@@ -69,30 +83,5 @@ function getLatLon(event) {
         )
 }
 
-function forecast(day) {
-    var dt_txt = day.dt_txt.split(" ")
-    var date = dt_txt[0];
-    var windSpeed = day.wind.speed;
-    var temperature = day.main.temp;
-    var humidity = day.main.humidity;
-    console.log(date, windSpeed, temperature, humidity);
-
-}
-
 searchButton.addEventListener('click', getLatLon);
 
-
-// http://api.openweathermap.org/data/2.5/forecast?id=524901&appid=c2497406e274517dafa1edd628b99c41
-// * get search term
-// * send search to API
-// * get results from API
-// * render results to result container
-// *  - each result should include
-// *  - temp
-// *  - weather,
-//      temp,
-//      humidity,
-//      wind speed
-// * - New results should clear old results
-// * - Once results are located, the search term should be emptied
-// * - search revealed below search button
