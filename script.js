7f26bb2bb44e484d05398c9f06f6852f28fb16c8
Creature Feature <ivy.mccurdy@gmail.com>
2023-04-26 19:12:27 -0500

    changed class names in html to better target them in css, reformatted the text, added icons in five day forecast

--- script.js
+++ script.js
@@ -24,16 +24,18 @@ function getFiveDay(id) {
          var formattedDate = new Date(forecastDate).toLocaleDateString('en-US', {weekday: 'short', month: 'short', day: 'numeric'});
          var forecastTemp = forecastData.main.temp.toFixed(0) + '°F';
          var forecastWind = forecastData.wind.speed + ' mph';
+         var forecastIcon = forecastData.weather[0].icon;
          var forecastHumidity = forecastData.main.humidity + '%';
 
          var forecastCard = $('<div>').addClass('card');
          var forecastCardBody = $('<div>').addClass('card-body');
          var forecastDateEl = $('<h5>').addClass('card-title').text(formattedDate);
+         var forecastCode =  $('<img>').addClass('card-text').attr('src', 'http://openweathermap.org/img/wn/' + forecastIcon + '.png').attr('alt', 'Weather icon').attr('style', 'width: 100px', 'height: 100px');
          var forecastTempEl = $('<p>').addClass('card-text').text('Temperature: ' + forecastTemp);
          var forecastWindEl = $('<p>').addClass('card-text').text('Wind Speed: ' + forecastWind);
          var forecastHumidityEl = $('<p>').addClass('card-text').text('Humidity: ' + forecastHumidity);
-         forecastCardBody.append(forecastDateEl, forecastTempEl, forecastWindEl, forecastHumidityEl);
-         forecastCard.append(forecastCardBody);
+         forecastCardBody.append(forecastDateEl, forecastCode, forecastTempEl, forecastWindEl, forecastHumidityEl);
+         forecastCard.append(forecastCardBody, forecastCode);
          $('#forecast').append(forecastCard);
         }
         
@@ -50,15 +52,21 @@ function getWeatherResults(lat, lon) {
         })
         .then(function (data) {
             console.log(data);
-            var card = $('<div>').addClass('card').attr('style', 'width: 30vw')
+            var card = $('<div>').addClass('card').attr('style', 'width: 20vw')
             var cardTitle = $('<h3>').addClass('card-title').text(data.name)
             var cardBody = $('<div>').addClass('card-body')
+            var iconCode =  $('<img>').addClass('card-text').attr('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png').attr('alt', 'Weather icon').attr('style', 'width: 100px', 'height: 100px');
             var tempEl = $('<p>').addClass('card-text').text('Temp: ' + data.main.temp.toFixed(0) + "°F")
             var windEl = $('<p>').addClass('card-text').text('Wind Speed: ' + data.wind.speed+ " mph")
             var humidityEl = $('<p>').addClass('card-text').text('Humidity: ' + data.main.humidity+ "%")
-            var iconCode =  $('<img>').addClass('card-img-top').attr('src', 'http://openweathermap.org/img/wn/' + data.weather[0].icon + '.png').attr('alt', 'Weather icon').attr('style', 'width: 55px', 'height: 55px');
             $('.weather').append(card.append(cardTitle, iconCode, cardBody, tempEl, windEl, humidityEl))
             getFiveDay(data.id);
+
+            var fiveCard=$('<div>').addClass('card')
+            var fiveCardBody=$('<div>').addClass('card-body')
+            var fiveDayTitle=$('<h4>').addClass('five-day').text('5 Day Forecast:');
+            fiveCard.append(fiveCardBody);
+            $('.five-day').append(fiveDayTitle);
         }
         )
 
@@ -77,7 +85,10 @@ function getLatLon(event) {
             var lat = data[0].lat
             var lon = data[0].lon
             $('.weather').empty();
+            $('.five-day').empty();
+            $('#forecast').empty();
             getWeatherResults(lat, lon);
+          
 
         }
         )
